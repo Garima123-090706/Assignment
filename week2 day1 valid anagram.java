c0892d211f54fded9b70ee780acaad1090409f46
Garima123-090706 <pandeygarima518@gmail.com>
2026-06-10 12:24:10 +0530

    Create week2 day1 valid anagram.java

---
+++ week2 day1 valid anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int arr[]=new int [26];
+
+         for(char a:s.toCharArray()){
+            arr[a-'a']++;
+         }
+
+         for(char b:t.toCharArray()){
+            arr[b-'a']--;
+         } 
+
+         for(int i:arr){
+            if(i!=0){
+                return false;
+            }
+         }
+         return true;
+    }
+}

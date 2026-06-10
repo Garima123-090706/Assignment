95e1b3cf348e417b4f12751df0324660b23a160e
Garima123-090706 <pandeygarima518@gmail.com>
2026-06-10 14:33:21 +0530

    Create week2 day3 implementstrStr.java

---
+++ week2 day3 implementstrStr.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int m=haystack.length();
+        int n=needle.length();
+         
+         if(needle.isEmpty()){
+            return 0;
+         }
+         if(m<n){
+            return -1;
+         }
+         for(int i =0;i<=m-n;i++){
+            if(haystack.substring(i,i+n).equals(needle)){
+                return i;
+            }
+         }
+         return -1;
+    }
+}
+
